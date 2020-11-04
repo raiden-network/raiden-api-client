371fad7e3efb81a81e598a6129e3554990060352
oliver <oliver@brainbot.com>
2020-11-04 17:31:51 +0100

    Added query requests

--- src/py_raiden_proxy/RaidenAPIWrapper.py
+++ src/py_raiden_proxy/RaidenAPIWrapper.py
@@ -1,8 +1,10 @@
 from json import JSONDecodeError
 
 import requests
+from typing import List
 from attrdict import AttrDict
-from py_raiden_proxy.exceptions.exceptions import RaidenAPIException, RaidenAPIConflictException
+from py_raiden_proxy.exceptions.exceptions import RaidenAPIException, RaidenAPIConflictException, \
+    InvalidAPIResponse
 from requests import RequestException
 
 
@@ -11,7 +13,24 @@ class RaidenAPIWrapper:
         self.api = f"http://{ip}:{port}/api/{version}/"
         self.headers = {'Content-Type': 'application/json', }
 
-    def open_channel(self, target, deposit, token, settle_timeout=500):
+    def get_channels(self) -> List[AttrDict]:
+        res = requests.get(f"{self.api}channels")
+        return self.handle_response(res)
+
+    def get_address(self) -> AttrDict:
+        res = requests.get(f"{self.api}address")
+        return self.handle_response(res)
+
+    def get_payments(self, target=None, token=None) -> List[AttrDict]:
+        # Query all payments
+        if None not in (target, token):
+            res = requests.get(f"{self.api}payments/{token}/{target}")
+        # Query payments with specific partner and token
+        else:
+            res = requests.get(f"{self.api}payments")
+        return self.handle_response(res)
+
+    def open_channel(self, target, deposit, token, settle_timeout=500) -> AttrDict:
         json_data = {
             "partner_address": target,
             "settle_timeout": settle_timeout,
@@ -25,9 +44,9 @@ class RaidenAPIWrapper:
             json=json_data,
             timeout=600
         )
-        return self.check_http_code(res)
+        return self.handle_response(res)
 
-    def fund_channel(self, target, deposit, token):
+    def fund_channel(self, target, deposit, token) -> AttrDict:
         json_data = {"total_deposit": deposit}
 
         res = requests.patch(
@@ -37,9 +56,9 @@ class RaidenAPIWrapper:
             timeout=600,
         )
 
-        return self.check_http_code(res)
+        return self.handle_response(res)
 
-    def transfer(self, target, amount, token, identifier=None, ):
+    def transfer(self, target, amount, token, identifier=None, ) -> AttrDict:
         json_data = {'amount': amount, }
 
         if identifier:
@@ -51,13 +70,23 @@ class RaidenAPIWrapper:
             json=json_data
         )
 
-        return self.check_http_code(res)
+        return self.handle_response(res)
 
-    def check_http_code(self, response):
+    def handle_response(self, response):
+
         try:
-            decoded_response = AttrDict(response.json())
-        except (KeyError, JSONDecodeError):
-            raise RaidenAPIException()
+            response_json = response.json()
+        except JSONDecodeError:
+            raise InvalidAPIResponse()
+
+        # For some get endpoints the result will be a list of jsons
+        if isinstance(response_json, list):
+            decoded_response = []
+            for item in response_json:
+                decoded_response.append(AttrDict(item))
+        # If it's a single dict
+        else:
+            decoded_response = AttrDict(response_json)
 
         # Successful request
         if 199 < response.status_code < 300:
@@ -68,6 +97,7 @@ class RaidenAPIWrapper:
             response.raise_for_status()
         except RequestException as ex:
             if response.status_code == 409:
-                raise RaidenAPIConflictException(decoded_response["errors"], response.status_code) from ex
+                raise RaidenAPIConflictException(decoded_response["errors"],
+                                                 response.status_code) from ex
             else:
                 raise RaidenAPIException(decoded_response["errors"], response.status_code) from ex
--- src/py_raiden_proxy/example/example.py
+++ src/py_raiden_proxy/example/example.py
@@ -8,13 +8,36 @@ TARGET = "0x1F916ab5cf1B30B22f24Ebf435f53Ee665344Acf"  # Raiden Hub
 
 rdn = RaidenAPIWrapper(ip=URL, port=PORT)
 
-try:
-    # Try to send payment to Raiden hub
-    result = rdn.transfer(amount=1, target=TARGET, token=TOKEN)
-    print(result)
 
-# If no path to Hub exists open new channel
-except RaidenAPIConflictException as e:
-    print("Looks like we have to open a new channel!")
+def transfer():
+    try:
+        # Try to send payment to Raiden hub
+        result = rdn.transfer(amount=1, target=TARGET, token=TOKEN)
+        print(result)
+
+    # If no path to receiver exists open new channel
+    except RaidenAPIConflictException:
+        print("Transfer not successful - No Path to receiver found")
+
+
+def open_channel():
     rdn.open_channel(target=TARGET, deposit=1000, token=TOKEN)
 
+
+def get_channel():
+    channels = rdn.get_channels()
+    for channel in channels:
+        print(channel)
+
+
+def get_payments():
+    print(f"List of all payments: {rdn.get_payments()}")
+    print(f"List of payments with Hub: {rdn.get_payments(target=TARGET, token=TOKEN)}")
+
+
+def get_address():
+    print(rdn.get_address().our_address)
+
+
+if __name__ == "__main__":
+    get_address()
