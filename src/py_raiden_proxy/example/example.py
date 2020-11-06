0467fb80113c955ab1139ea58b5ac4f569eb294e
oliver <oliver@brainbot.com>
2020-11-06 16:59:19 +0100

    Added tests

--- src/py_raiden_proxy/example/example.py
+++ src/py_raiden_proxy/example/example.py
@@ -1,9 +1,10 @@
-from py_raiden_proxy.RaidenAPIWrapper import RaidenAPIWrapper
+from py_raiden_proxy.wrapper import RaidenAPIWrapper
 from py_raiden_proxy.exceptions.exceptions import RaidenAPIException, RaidenAPIConflictException
 
 URL = "localhost"
 PORT = "5001"
 TOKEN = "0x3ed0DaEDC3217615bde34FEDd023bC81ae49251B"
+NEW_TOKEN = "0x7358Dd6Ebce0b9311CEBf8211CCa53230abEA8f8"
 PARTNER = "0x1F916ab5cf1B30B22f24Ebf435f53Ee665344Acf"  # Raiden Hub
 
 rdn = RaidenAPIWrapper(ip=URL, port=PORT)
@@ -24,8 +25,13 @@ def open_channel():
     rdn.open_channel(partner=PARTNER, deposit=1000, token=TOKEN)
 
 
-def get_channel():
-    channels = rdn.get_channels()
+def get_channel(token=None, partner=None):
+    if token and partner:
+        channels = rdn.get_channels(token, partner)
+    elif token:
+        channels = rdn.get_channels(token)
+    else:
+        channels = rdn.get_channels()
     for channel in channels:
         print(channel)
 
@@ -39,5 +45,44 @@ def get_address():
     print(rdn.get_address().our_address)
 
 
+def get_token_network():
+    print(rdn.get_token_network())
+    print(rdn.get_token_network(token=TOKEN))
+
+
+def get_raiden_version():
+    print(rdn.get_raiden_version())
+
+
+def get_pending_transfer():
+    print(rdn.get_pending_transfer())
+
+
+def get_connections():
+    print(rdn.get_connections())
+
+
+def get_node_status():
+    print(rdn.get_node_status())
+
+
+def leave_token_network(token):
+    print(rdn.leave_token_network(token))
+
+
+def register_token(token):
+    print(rdn.register_token(token=token))
+
+
+def fund_channel(partner, token, deposit):
+    print(rdn.fund_channel(partner=partner, token=token, deposit=deposit))
+
+
+def mint_tokens(token, receiver, amount):
+    print(rdn.mint_tokens(token=token, receiver=receiver, amount=amount))
+
+
 if __name__ == "__main__":
-    get_address()
+    amount = 1000
+    print(f"Minting {amount} Tokens for {PARTNER}")
+    mint_tokens(token=TOKEN, receiver=PARTNER, amount=1000)
--- tests/test_RaidenAPIWrapper.py
+++ tests/test_RaidenAPIWrapper.py
@@ -1,6 +1,11 @@
+import json
+
 import pytest
-from py_raiden_proxy.RaidenAPIWrapper import RaidenAPIWrapper
-from py_raiden_proxy.exceptions.exceptions import RaidenAPIException
+import responses
+from requests.models import Response
+from py_raiden_proxy.wrapper import RaidenAPIWrapper
+from py_raiden_proxy.exceptions.exceptions import RaidenAPIException, RaidenAPIConflictException, \
+    InvalidAPIResponse
 
 URL = "localhost"
 PORT = "5001"
@@ -12,11 +17,60 @@ WRONG_FORMATED_TARGET = "0x000000000000000000000000000000000000000"
 rdn = RaidenAPIWrapper(ip=URL, port=PORT)
 
 
-class TestTransfer:
-    def test_not_found_error(self):
-        with pytest.raises(RaidenAPIException):
-            rdn.transfer(amount=1, target=WRONG_FORMATED_TARGET, token=TOKEN)
+def default_response():
+    res = Response()
+    res._content = b'{"value": "Some value"}'
+    res.status_code = 200
+    return res
 
-    def test_conflict_error(self):
-        with pytest.raises(RaidenAPIException):
-            rdn.transfer(amount=1, target=OFFLINE_TARGET, token=TOKEN)
+
+class TestHandleResponse:
+
+    def test_error_status_codes(self):
+        res = default_response()
+        res._content = b'{"errors": "Some Error"}'
+        for status_code in [402, 403, 404, 409, 500, 501, 503]:
+            res.status_code = status_code
+            if status_code == 409:
+                with pytest.raises(RaidenAPIConflictException):
+                    rdn._handle_response(res)
+
+            else:
+                with pytest.raises(RaidenAPIException):
+                    rdn._handle_response(res)
+
+    def test_expected_status_codes(self):
+        res = default_response()
+        for status_code in [200, 201]:
+            res.status_code = status_code
+            assert rdn._handle_response(res)["value"] == "Some value"
+
+    def test_attrdict_response(self):
+        res = default_response()
+        assert rdn._handle_response(res).value == "Some value"
+
+    def test_list_of_attrdict_response(self):
+        res = default_response()
+        res._content = b'[{"value1": "Some value"}, {"value2": "Another value"}]'
+        assert rdn._handle_response(res)[0].value1 == "Some value"
+        assert rdn._handle_response(res)[1].value2 == "Another value"
+
+    def test_list_of_string_response(self):
+        res = default_response()
+        res._content = b'["string1", "string2"]'
+        assert rdn._handle_response(res)[0] == "string1"
+        assert rdn._handle_response(res)[1] == "string2"
+
+    def test_invalid_api_response(self):
+        res = default_response()
+        res._content = b'not valid json'
+        with pytest.raises(InvalidAPIResponse):
+            rdn._handle_response(res)
+
+
+class TestRequests:
+    def __int__(self):
+        self.api = "http://{URL}:{PORT}/api/v1/"
+
+
+
